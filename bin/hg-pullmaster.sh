8afd8429e0e4610b6ab7eca10fbcfd08df662363
Tony Tung <ttung@chanzuckerberg.com>
2018-09-14 12:34:25 -0700

    enhancements for pullmaster

--- bin/hg-pullmaster.sh
+++ bin/hg-pullmaster.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+set -x
+
 hg boo -d $(hg boo -q | egrep -v '(^tonytung|^master$)')
 hg pull --hidden
 hg boo -d $(hg oldbm)
-hg strip -r 'obsolete() & (!ancestors(!author(ttung)) & !ancestors(master))'
+#hg strip -r 'obsolete() & (!ancestors(!author(ttung)) & !ancestors(master))'
 
 set -e
 
-for rev in $(hg log -T '{node}\n' -r 'children(ancestors(master)) & (!ancestors(master)) & (author(ttung) | author(tonytung))'); do
+for rev in $(hg log -T '{node}\n' -r 'children(ancestors(master)) & (!ancestors(master)) & (!obsolete()) & (author(ttung) | author(tonytung))'); do
     echo "Considering $rev..."
     if [ "$(hg log -r 'descendants('$rev') & !(author(ttung) | author(tonytung))')" != "" ]; then
         # has descendants that are not authored by me.
         hg rebase --keep -d master -r "$rev::(descendants($rev) & (author(ttung) | author(tonytung)))"
+        for rebasedrev in $(hg log -T '{node}\n' -r "$rev::(descendants($rev) & (author(ttung) | author(tonytung)))"); do
+            hg debugobsolete $rebasedrev
+        done
     else
         hg rebase -d master -s $rev
     fi
@@ -20,5 +25,10 @@ done
 set +e
 
 hg boo -d $(hg oldbm)
-#hg strip -r 'obsolete() or unstable()'
-hg strip -r 'not (bookmark() or ancestors(bookmark()) or author(ttung))'
+
+# examine all the revs that are not in the master lineage, and are not part of a bookmark's history.
+for rev in $(hg log -T '{node}\n' -r 'children(ancestors(master)) & (!ancestors(master)) & (not (bookmark() or ancestors(bookmark())))'); do
+    if [ "$(hg log -r 'descendants('$rev') & !(author(ttung) | author(tonytung))')" == "" ]; then
+        hg strip $rev
+    fi
+done
