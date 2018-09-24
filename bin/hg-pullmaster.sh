e60bba9291d18ccdb7d1ae689980c129c9247e65
Tony Tung <ttung@chanzuckerberg.com>
2018-09-24 13:13:54 -0700

    improvements to hg-syncmaster.sh

--- bin/hg-pullmaster.sh
+++ bin/hg-pullmaster.sh
@@ -3,22 +3,34 @@
 set -x
 
 hg boo -d $(hg boo -q | egrep -v '(^tonytung|^master$)')
-hg pull --hidden
+hg boo -d $(hg log -T "{join(bookmarks, '\n')}\n" -r 'obsolete() & (author(ttung) | author(tonytung))')
+hg pull
 hg boo -d $(hg oldbm)
 #hg strip -r 'obsolete() & (!ancestors(!author(ttung)) & !ancestors(master))'
 
 set -e
 
-for rev in $(hg log -T '{node}\n' -r 'children(ancestors(master)) & (!ancestors(master)) & (!obsolete()) & (author(ttung) | author(tonytung))'); do
+for rev in $(hg log -T '{node}\n' -r 'children(ancestors(master)) & (!ancestors(master)) & (author(ttung) | author(tonytung))'); do
     echo "Considering $rev..."
     if [ "$(hg log -r 'descendants('$rev') & !(author(ttung) | author(tonytung))')" != "" ]; then
         # has descendants that are not authored by me.
-        hg rebase --keep -d master -r "$rev::(descendants($rev) & (author(ttung) | author(tonytung)))"
-        for rebasedrev in $(hg log -T '{node}\n' -r "$rev::(descendants($rev) & (author(ttung) | author(tonytung)))"); do
-            hg debugobsolete $rebasedrev
-        done
+        if [ "$(hg log -r '('$rev' & !obsolete())')" != "" ]; then
+            # not obsolete, rebase
+            hg rebase --keep -d master -r "$rev::(descendants($rev) & (author(ttung) | author(tonytung)))"
+            for rebasedrev in $(hg log -T '{node}\n' -r "$rev::(descendants($rev) & (author(ttung) | author(tonytung)))"); do
+                hg debugobsolete $rebasedrev
+            done
+        fi
     else
-        hg rebase -d master -s $rev
+        # strip everything that's obsolete first.
+        while true; do
+            hg strip -r 'descendants('$rev') & obsolete() & head()' || break
+        done
+
+        # we may have stripped everything, so make sure something is still there before continuing.
+        if [ "$(hg log --hidden -r '('$rev' & !(hidden()))')" != "" ]; then
+            hg rebase -d master -r 'descendants('$rev') & (!obsolete())'
+        fi
     fi
 done
 
@@ -27,8 +39,6 @@ set +e
 hg boo -d $(hg oldbm)
 
 # examine all the revs that are not in the master lineage, and are not part of a bookmark's history.
-for rev in $(hg log -T '{node}\n' -r 'children(ancestors(master)) & (!ancestors(master)) & (not (bookmark() or ancestors(bookmark())))'); do
-    if [ "$(hg log -r 'descendants('$rev') & !(author(ttung) | author(tonytung))')" == "" ]; then
-        hg strip $rev
-    fi
-done
+hg strip -r 'children(ancestors(master) & !master) & (!ancestors(master)) & (not (bookmark() or ancestors(bookmark())))'
+
+exit 0
